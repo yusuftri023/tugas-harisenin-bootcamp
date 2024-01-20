ef1a993549944475040aef587536591315f1668a
yusuf <yusuftri023@gmail.com>
2024-01-21 00:37:44 +0700

    init: project

---
+++ tugas html css/test.js
@@ -0,0 +1,4 @@
+document.querySelector(".haha").addEventListener("click", () => {
+  console.log("dropdown nav dropped");
+  document.querySelector(".dropdown").setAttribute("class", "dropdown");
+});
